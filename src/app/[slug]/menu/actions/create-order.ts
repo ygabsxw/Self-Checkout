944c90800899a83afa684053ec85bb291ed134ac
ygabsxw <gabrieldrvianna@gmail.com>
2025-03-03 18:44:11 -0300

    chore: fix name error

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
@@ -58,7 +59,7 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
-  revalidadePath(`/${input.slug}/ orders`);
+  revalidatePath(`/${input.slug}/ orders`);
   redirect(
     `/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`,
   );
